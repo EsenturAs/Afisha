8498c73af91e3c52394aaeb74c37c89c3e3789e0
EsenturAs <esentur55555@gmail.com>
2024-12-13 16:20:20 +0600

    Added CRUD system

--- afisha/urls.py
+++ afisha/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('api/v1/movies/', views.movie_list_view),
     path('api/v1/movies/reviews/', views.reviews_for_movie_list_view),
     path('api/v1/reviews/', views.review_list_view),
-    path('api/v1/directors/<int:id>/', views.movie_detail_view),
+    path('api/v1/directors/<int:id>/', views.director_detail_view),
     path('api/v1/movies/<int:id>/', views.movie_detail_view),
-    path('api/v1/reviews/<int:id>/', views.movie_detail_view),
+    path('api/v1/reviews/<int:id>/', views.review_detail_view),
 ]
